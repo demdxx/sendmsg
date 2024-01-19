55495f0e0a1cac9405494a08062ee5952aca3bc5
Dmitry Ponomarev <demdxx@gmail.com>
2024-01-19 16:10:28 +0100

    Fix template render method

--- template/template.go
+++ template/template.go
@@ -1,11 +1,14 @@
 package template
 
-import "context"
+import (
+	"context"
+	"io"
+)
 
 // Template of the message to send
 type Template interface {
 	Name() string
 	IsHTML() bool
 	Subject(ctx context.Context, vars map[string]any) (string, error)
-	Render(ctx context.Context, vars map[string]any) (string, error)
+	Render(ctx context.Context, wr io.Writer, vars map[string]any) error
 }
--- template_message.go
+++ template_message.go
@@ -1,7 +1,9 @@
 package sendmsg
 
 import (
+	"bytes"
 	"context"
+	"io"
 
 	"github.com/demdxx/xtypes"
 )
@@ -9,7 +11,7 @@ import (
 type itemplater interface {
 	IsHTML() bool
 	Subject(ctx context.Context, vars map[string]any) (string, error)
-	Render(ctx context.Context, vars map[string]any) (string, error)
+	Render(ctx context.Context, wr io.Writer, vars map[string]any) error
 }
 
 type TemplateMessage struct {
@@ -32,16 +34,25 @@ func (m *TemplateMessage) GetSubject(ctx context.Context, vars map[string]any) (
 
 func (m *TemplateMessage) GetHTML(ctx context.Context, vars map[string]any) (string, error) {
 	if m.Template != nil && m.Template.IsHTML() {
-		return m.Template.Render(ctx, xtypes.Map[string, any](m.Vars).Merge(vars))
+		return m.renderTmpl(ctx, vars)
 	}
 	return "", nil
 }
 
 func (m *TemplateMessage) GetPlainText(ctx context.Context, vars map[string]any) (string, error) {
 	if m.Template != nil && !m.Template.IsHTML() {
-		return m.Template.Render(ctx, xtypes.Map[string, any](m.Vars).Merge(vars))
+		return m.renderTmpl(ctx, vars)
 	}
 	return "", nil
+}
+
+func (m *TemplateMessage) renderTmpl(ctx context.Context, vars map[string]any) (string, error) {
+	var buf bytes.Buffer
+	err := m.Template.Render(ctx, &buf, xtypes.Map[string, any](m.Vars).Merge(vars))
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
 }
 
 func (m *TemplateMessage) Complete() error { return nil }
